08b1ffe28c936132822fdd31b94138e32be9f887
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-09 09:21:09 +0200

    V2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>partie 5 exercice 1</title>
+        <title>partie 5 exercice 2</title>
     </head>
     <body>
         <p> <?php echo $month[2] ?> </p>
